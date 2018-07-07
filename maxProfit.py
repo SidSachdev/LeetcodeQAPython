1849167ff53f39eb6a24d0da308981a9209805b8
Sid Sachdev <sid.sachdev9@yahoo.ca>
2018-07-07 19:39:14 -0400

    updated MaxProfit with multiple transactions

--- maxProfit.py
+++ maxProfit.py
@@ -1,4 +1,4 @@
-# Leetcode MaxProfit I
+# Leetcode MaxProfit I and II
 # Buying and selling stock
 # One time only
 
@@ -28,3 +28,22 @@ class Solution:
         return maxSoFar
 
     maxProfit([1,2,3,4,75,3,49,1])
+
+
+
+
+    def multipleTransactions(prices):
+        if not prices or len(prices) is 1:
+            return 0
+
+        profit = 0
+
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+
+        return (profit)
+
+    maxProfit([1, 2, 4])
+
+print(Solution.multipleTransactions([1, 2, 4]))
